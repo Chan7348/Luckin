6eab32f7b6f399e2440ff64e88699af2607e706b
Chan <add7348@outlook.com>
2024-01-31 14:51:56 +0800

    declare the mapping visibility explicitly

--- contracts/Luckin.sol
+++ contracts/Luckin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Luckin{
-    mapping(uint256 => address) idToAddress;
+    mapping(uint256 => address) public idToAddress;
     uint256 private totalSupply;
     uint256 private id;
 
