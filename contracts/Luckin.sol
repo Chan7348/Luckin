25ea648a06b734d02a7f087377b01cb63d77486e
Chan <add7348@outlook.com>
2024-01-31 15:20:36 +0800

    add docstrings

--- contracts/Luckin.sol
+++ contracts/Luckin.sol
@@ -6,23 +6,31 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 // title: Luckin
 // @custom:security-contact: add7348@outlook.com
 contract Luckin{
+    // link the order
     mapping(uint256 => address) public idToAddress;
+
+    // current totalsupply
     uint256 private totalSupply;
+    // the first coffee to sale, when it's been sold, id++
     uint256 private id;
 
+    // broadcast a coffee purchase
     event coffeePurchased(address indexed buyer, uint256 indexed coffeeId);
 
+    // error
     error noMoreCoffee();    
 
-
+    // make one more coffee
     function makeCoffee() external {
         totalSupply++;
     }
 
+    // return if there is spare coffee
     function haveSpareCoffee() external view returns (bool) {
        return id < totalSupply;
     }
 
+    // buy coffee
     function buyCoffee() external {
         // require(id < totalSupply, "No more coffee!!!");
         if (id < totalSupply) revert noMoreCoffee();
